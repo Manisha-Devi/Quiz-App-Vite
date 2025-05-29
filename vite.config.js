2945c6ae15218b7933b982b27d8dd287d1645350
Manisha-Devi <manishadevi1052000@gmail.com>
2025-05-29 08:36:31 +0000

    First Replit Test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      "03eba55e-d242-4045-a19c-cd64d8fd31a6-00-3tp4wr24lokel.pike.replit.dev",
+      // Add any other hosts you want to allow here
+    ],
+  },
+});
